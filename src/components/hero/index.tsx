a86eee70db81a02bad1b9f838c4dfe1298ec7219
Fernando <fernandoeqp59@gmail.com>
2025-06-10 14:29:48 -0300

    refeito get com fetch

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { getMovies } from "@/utils/actions/api";
 import { MoviesProps } from "@/utils/type";
 import MovieOverview from "./movieOveriew";
-import Link from "next/link";
 
 export default async function Hero() {
   const movieRandom: MoviesProps[] = await getMovies();
@@ -16,39 +15,37 @@ export default async function Hero() {
   const movie: MoviesProps = movieRandom[random];
 
   return (
-    <Link href={`/movie/${movie.id}`} className="cursor-default">
-      <section
-        className="w-full flex justify-center relative h-170 mt-15 
+    <section
+      className="w-full flex justify-center relative h-170 mt-15 
       max-sm:h-full
       "
-      >
-        <div
-          className="w-9/10 max-w-550 h-full relative 
+    >
+      <div
+        className="w-9/10 max-w-550 h-full relative 
         max-sm:w-full max-sm:h-100 
         "
-        >
-          <Image
-            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-            alt="Imagem de fundo"
-            quality={100}
-            priority={true}
-            fill
-            className=" rounded-2xl shadow-2xl shadow-gray-500 absolute opacity-60 
+      >
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+          alt="Imagem de fundo"
+          quality={100}
+          priority={true}
+          fill
+          className=" rounded-2xl shadow-2xl shadow-gray-500 absolute opacity-60 
           max-sm:rounded-none max-sm:object-cover max-sm:shadow-none
           "
-          />
-          <div className="absolute w-full h-full ">
-            <h1
-              className="absolute bottom-10 left-10 font-bold text-3xl text-white 
+        />
+        <div className="absolute w-full h-full ">
+          <h1
+            className="absolute bottom-10 left-10 font-bold text-3xl text-white 
             max-sm:text-2xl max-sm:bottom-4
             "
-            >
-              {movie.title}
-            </h1>
-            <MovieOverview overview={movie.overview} />
-          </div>
+          >
+            {movie.title}
+          </h1>
+          <MovieOverview overview={movie.overview} />
         </div>
-      </section>
-    </Link>
+      </div>
+    </section>
   );
 }
--- src/utils/actions/api.tsx
+++ src/utils/actions/api.tsx
@@ -1,38 +1,49 @@
-import axios from "axios";
+const API_URL = process.env.NEXT_PUBLIC_TMDB_API_URL;
+const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
+if (!API_URL || !API_KEY) {
+  throw new Error("API_URL ou API_KEY não definido");
+}
+
+// Função para buscar os filmes em cartaz
 export async function getMovies() {
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_TMDB_API_URL}/movie/now_playing`,
-      {
-        params: {
-          api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
-        },
-      }
+    const response = await fetch(
+      `${API_URL}/movie/now_playing?api_key=${API_KEY}&language=pt-BR`
     );
-    return response.data.results;
+
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar filmes: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.results || [];
   } catch (error) {
     console.error("Erro ao buscar filmes:", error);
     return [];
   }
 }
 
-// Get pelo id
+// Função para buscar detalhes de um filme
 export async function getMovieDetails(id: number) {
+  if (!id) {
+    console.error("ID do filme não fornecido");
+    return null;
+  }
+
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_TMDB_API_URL}/movie/${id}
-      `,
-      {
-        params: {
-          api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
-          language: "pt-BR",
-        },
-      }
+    const response = await fetch(
+      `${API_URL}/movie/${id}?api_key=${API_KEY}&language=pt-BR`
     );
-    return response.data;
+
+    if (!response.ok) {
+      throw new Error(`Erro ao buscar detalhes do filme: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error("Erro ao buscar detalhes do filme:", error);
-    return;
+    return null;
   }
 }
